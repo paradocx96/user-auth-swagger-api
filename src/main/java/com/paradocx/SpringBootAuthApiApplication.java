73faaf117b0c294d2ce3240b7ed6b2719f853687
Navinda Chandrasiri <adilanga@gmail.com>
2023-10-28 22:48:38 +0530

    Project Init

---
+++ src/main/java/com/paradocx/SpringBootAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.paradocx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paradocx/SpringBootAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradocx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
